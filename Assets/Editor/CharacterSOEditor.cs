4fe0460316752a40390dd24094220139fcafc8e9
Mustafa Kaan Güngör <kaan062004+github@gmail.com>
2026-05-10 21:02:44 +0300

    added editor scripts for scriptable objects

--- Assets/Editor/CharacterSOEditor.cs
+++ Assets/Editor/CharacterSOEditor.cs
@@ -1,16 +1,81 @@
+using DialogSystem.Dialogue;
+using UnityEditor;
 using UnityEngine;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 
-public class CharacterSOEditor : MonoBehaviour
+[CustomEditor(typeof(CharacterSO))]
+public class CharacterSOEditor : Editor
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private CharacterSO characterSO;
+
+    private void OnEnable()
     {
-        
+        characterSO = (CharacterSO)target;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnInspectorGUI()
     {
-        
+        DrawValidationBanner();
+        DrawDefaultInspector();
+        DrawQuickCreateButton();
+    }
+
+    private void DrawValidationBanner()
+    {
+        if (string.IsNullOrWhiteSpace(characterSO.characterName))
+        {
+            EditorGUILayout.HelpBox("Character name is empty.", MessageType.Error);
+        }
+
+        if (characterSO.dialogues == null || characterSO.dialogues.Length == 0)
+        {
+            EditorGUILayout.HelpBox("No dialogues assigned.", MessageType.Error);
+        } else
+        {
+            bool hasNull = false;
+            for (int i = 0; i < characterSO.dialogues.Length; i++)
+            {
+                if (characterSO.dialogues[i] == null)
+                {
+                    hasNull = true;
+                    break;
+                }
+            }
+
+            if(hasNull)
+            {
+                EditorGUILayout.HelpBox("Some dialogue slots are missing", MessageType.Warning);
+            } 
+        }
+    }
+
+    private void DrawQuickCreateButton()
+    {
+        EditorGUILayout.Space(5);
+
+        if(GUILayout.Button("Create New Dialogue", GUILayout.Height(30)))
+        {
+            DialogueSO newDialogue = CreateInstance<DialogueSO>();
+            newDialogue.dialogueLines = new List<string>
+            {
+                "New Dialogue Line"
+            };
+
+            string assetPath = $"Assets/Data/{characterSO.name}_Dia_{characterSO.dialogues.Length + 1}.asset";
+
+            assetPath = AssetDatabase.GenerateUniqueAssetPath(assetPath);
+
+            AssetDatabase.CreateAsset(newDialogue, assetPath);
+            AssetDatabase.SaveAssets();
+
+            System.Array.Resize(ref characterSO.dialogues, characterSO.dialogues.Length + 1);
+            characterSO.dialogues[^1] = newDialogue;
+
+            EditorUtility.SetDirty(characterSO);
+            EditorSceneManager.MarkSceneDirty(EditorSceneManager.GetActiveScene());
+
+            AssetDatabase.Refresh();
+        }
     }
 }
--- Assets/Editor/DialogueSOEditor.cs
+++ Assets/Editor/DialogueSOEditor.cs
@@ -1,16 +1,41 @@
-using UnityEngine;
+using DialogSystem.Dialogue;
+using UnityEditor;
 
-public class DialogueSOEditor : MonoBehaviour
+[CustomEditor(typeof(DialogueSO))]
+public class DialogueSOEditor : Editor
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    private DialogueSO dialogue;
+
+    void OnEnable()
     {
-        
+        dialogue = (DialogueSO)target;
     }
 
-    // Update is called once per frame
-    void Update()
+    public override void OnInspectorGUI()
     {
-        
+        DrawValidationBanner();
+        DrawDefaultInspector();
+    }
+
+    private void DrawValidationBanner()
+    {
+        if (dialogue.dialogueLines == null || dialogue.dialogueLines.Count == 0)
+        {
+            EditorGUILayout.HelpBox("No dialogue lines assigned.", MessageType.Error);
+        }
+        else
+        {
+            bool hasEmpty = false;
+            for (int i = 0; i < dialogue.dialogueLines.Count; i++)
+            {
+                if (string.IsNullOrWhiteSpace(dialogue.dialogueLines[i]))
+                {
+                    hasEmpty = true;
+                    break;
+                }
+            }
+            if (hasEmpty)
+                EditorGUILayout.HelpBox("Contains empty lines.", MessageType.Warning);
+        }
     }
 }
